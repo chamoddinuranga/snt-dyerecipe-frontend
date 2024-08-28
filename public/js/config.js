c6e19a7d4466fcc284137092a59e6c81d5a52e2a
chamoddinuranga <chamod3246@gmail.com>
2024-08-28 08:17:47 +0530

    if a 102 => 104

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // Base URL for the API
-  // API_BASE_URL: "http://192.168.8.102:8080", // Change this to your backend's URL
-  API_BASE_URL: "http://192.168.8.102:8080", // Change this to your backend's URL
+  // API_BASE_URL: "http://192.168.8.104:8080", // Change this to your backend's URL
+  API_BASE_URL: "http://192.168.8.104:8080", // Change this to your backend's URL
 };
 
 export default CONFIG;
